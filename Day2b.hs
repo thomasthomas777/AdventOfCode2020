3371566208c05174372c25ed65632d999e047ec2
Thomas Roadnight <tom@thomass-mbp.home>
2020-12-02 18:31:13 +0000

    Adding Regex Consolidated Version after earlier package manager failure. Requies Text.Regex.Posix library.

--- Day2b.hs
+++ Day2b.hs
@@ -1,64 +1,32 @@
 import System.IO
+import Text.Regex.Posix
+
+data ParsedComponents = ParsedComponents Int Int Char String
 
 strToInt :: String -> Int
 strToInt a = read a :: Int
 
-minVal :: String -> String -> Int
-minVal "" a = 0
-minVal (i:is) a
-    | i == '-'  = strToInt a
-    | otherwise = minVal is (a ++ [i])
-
-maxVal :: String -> String -> Bool -> Int
-maxVal "" _ _ = 0
-maxVal (i:is) a s
-    | i == '-'      = maxVal is a True
-    | i == ' '      = strToInt a
-    | s == False    = maxVal is a s
-    | otherwise     = maxVal is (a ++ [i]) s
-
-charCheck :: String -> Bool -> Char 
-charCheck "" _ = ' '
-charCheck (i:is) s
-    | i == ' '      = charCheck is True
-    | s == True     = i
-    | otherwise     = charCheck is False
-
-stringCheck :: String -> Bool -> String
-stringCheck "" _ = " "
-stringCheck (i:is) s
-    | i == ':'     = stringCheck is True
-    | s == True    = is -- Leading space
-    | otherwise    = stringCheck is False
-
-reduceString :: String -> Char -> Int -> Int -> Bool
-reduceString "" _ _ _ = False
-reduceString (s:ss) c cn it
-    | cn == it  = c == s
-    | otherwise = reduceString ss c cn (it+1)
-
-
 verifyString :: Int -> Int -> Char -> String -> Bool
-verifyString mi ma c s = (reduceString s c mi 1) /= (reduceString s c ma 1)
+verifyString mi ma c s = ((s !! (mi - 1)) == c) /= ((s !! (ma - 1)) == c)
 
-validPassword :: String -> Bool
-validPassword [] = False
-validPassword i 
+validPassword :: ParsedComponents -> Bool
+validPassword (ParsedComponents mi ma c s)
     | verifyString mi ma c s == True    = True
     | otherwise                         = False
-        where mi = minVal i ""
-              ma = maxVal i "" False
-              c = charCheck i False
-              s = stringCheck i False
               
+matchLine :: String -> [String]
+matchLine "" = [""]
+matchLine a = (a =~ "([0-9]+)-([0-9]+) ([a-z]): ([a-z]+)" :: [[String]]) !! 0
 
-conquerer :: (Eq a, Num a) => [String] -> a -> a
+parsed :: [String] -> ParsedComponents
+parsed xs = ParsedComponents (strToInt (xs!!1)) (strToInt (xs!!2)) (((xs!!3) !! 0)) (xs!!4)
+
+conquerer :: [String] -> Int -> Int
 conquerer [] v = v
 conquerer (i:is) v
     | r == True  = conquerer is v+1
     | otherwise  = conquerer is v
-        where r = validPassword i
-
+        where r = validPassword (parsed (matchLine i))
 main = do
     contents <- readFile "Day2Data.txt"
     print ( conquerer ( lines ( contents ) ) 0)
