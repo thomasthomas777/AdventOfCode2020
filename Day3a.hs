1c6b74aa924163e286258d7ec944105998723334
Thomas Roadnight <tom@thomass-mbp.home>
2020-12-03 06:45:10 +0000

    Day 3

---
+++ Day3a.hs
@@ -0,0 +1,18 @@
+import System.IO
+
+data Point = Point Int Int
+
+traverseSlope :: [String] -> Int -> Point -> Int
+traverseSlope [[]] t (Point a b) = t
+traverseSlope g t (Point a b)
+    | b >= maxy  = t
+    | currPos == '#'    = traverseSlope g (t+1) newPos
+    | otherwise         = traverseSlope g t newPos
+    where maxx = length (g !! 0)
+          maxy = length g
+          currPos = ((g !! b) !! a)
+          newPos = Point ((a+3) `mod` (length (g !! 0))) (b+1)
+
+main = do
+    contents <- readFile "Day3Data.txt"
+    print ( traverseSlope ( lines ( contents ) ) 0 (Point 0 0))
---
+++ Day3b.hs
@@ -0,0 +1,23 @@
+import System.IO
+
+data Point = Point Int Int
+
+traverseSlope :: [String] -> Int -> Point -> Point -> Int
+traverseSlope [[]] t (Point a b) (Point c d) = t
+traverseSlope g t (Point a b) (Point c d)
+    | b >= maxy  = t
+    | currPos == '#'    = traverseSlope g (t+1) newPos (Point c d)
+    | otherwise         = traverseSlope g t newPos (Point c d)
+    where maxx = length (g !! 0)
+          maxy = length g
+          currPos = ((g !! b) !! a)
+          newPos = Point ((a+c) `mod` (length (g !! 0))) (b+d)
+
+
+traverseIterations :: [String] -> [Point] -> Int
+traverseIterations [] [] = 0
+traverseIterations g xs = foldl (*) 1 [(traverseSlope g 0 (Point 0 0) p) | p <- xs]
+
+main = do
+    contents <- readFile "Day3Data.txt"
+    print ( traverseIterations ( lines ( contents ) ) [(Point 1 1), (Point 3 1), (Point 5 1), (Point 7 1), (Point 1 2)])
