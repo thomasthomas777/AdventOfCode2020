41542c3cf79f2c553779d1cead27dc36554db24e
Thomas Roadnight <tom@thomass-mbp.home>
2020-12-06 08:56:06 +0000

    Clean

--- Day6b.hs
+++ Day6b.hs
@@ -7,14 +7,8 @@ import Data.Set (toList, fromList)
 splitAndSetifyInput :: String -> [[String]]
 splitAndSetifyInput x = map (\y -> splitOn "\n" y) (splitOn "\n\n" x)
 
-intersection :: [String] -> String -> Int
-intersection (x:xs) s
-    | l == 0    = length $ intersect x s
-    | otherwise = intersection xs (intersect s x)
-        where l = length xs
-
 count :: [[String]] -> Int
-count = foldl (\x y -> ((intersection y (head y)) + x)) 0
+count = foldl (\x y -> (length $ foldr1 intersect y) + x) 0
 
 main = do
     contents <- readFile "Day6Data.txt"
