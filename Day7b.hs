52a882b9173440563cb981bb1b3967d25f5f25c8
Thomas Roadnight <tom@thomass-mbp.home>
2020-12-07 09:29:42 +0000

    Quick clean.

--- Day7b.hs
+++ Day7b.hs
@@ -7,17 +7,16 @@ data BagDependency = BagDependency String [(String, Int)]
 
 parseLine :: String -> String -> [(String, Int)] -> BagDependency
 parseLine s m r
-    | init_str /= []  =             parseLine (init_str !! 0 !! 2) ((init_str !! 0) !! 1) []
+    | init_str /= []  =             parseLine (head init_str !! 2) (head init_str !! 1) []
     | empty_bag /= [] =             BagDependency m r
-    | dependency_with_carry /= [] = parseLine (dependency_with_carry !! 0 !! 4) m new_r
+    | dependency_with_carry /= [] = parseLine (head dependency_with_carry !! 4) m new_r
     | dependency_no_carry /= [] =   BagDependency m new_rr
         where init_str = s =~ "^([a-z]+ [a-z]+) bags contain (.*)" :: [[String]]
               empty_bag = s =~ "no other bags." :: [[String]]
               dependency_with_carry = s =~ "([0-9]*) ([a-z]+ [a-z]+) (bag|bags), (.*)" :: [[String]]
               dependency_no_carry = s =~ "([0-9]*) ([a-z]+ [a-z]+) (bag|bags)." :: [[String]]
-              new_r = (r ++ [((dependency_with_carry !! 0) !! 2, (read ((dependency_with_carry !! 0) !! 1) :: Int))])
-              new_rr = (r ++ [((dependency_no_carry !! 0) !! 2, (read ((dependency_no_carry !! 0) !! 1) :: Int))])
-
+              new_r = (r ++ [(head dependency_with_carry !! 2, (read (head dependency_with_carry !! 1) :: Int))])
+              new_rr = (r ++ [(head dependency_no_carry !! 2, (read (head dependency_no_carry !! 1) :: Int))])
 
 identifyBag :: [BagDependency] -> String -> [(String, Int)]
 identifyBag ((BagDependency a b):xs) s
