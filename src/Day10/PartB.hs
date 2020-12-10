0d8080d8c69d112a1afbf40b7e96eb28c476dde4
Thomas Roadnight <tom@thomass-mbp.home>
2020-12-10 07:56:49 +0000

    Clean Day 10 Part B.

--- src/Day10/PartB.hs
+++ src/Day10/PartB.hs
@@ -6,10 +6,7 @@ calc :: [Int] -> Int -> (M.Map Int Int) -> Int
 calc [] p m = M.findWithDefault 0 p m   -- No need to consider last element as flat in number of ways derivable (last m + 3 only and always)
 calc (x:xs) p m = calc xs x new_map
                     where 
-                        a1 = M.findWithDefault 0 (x-1) m
-                        a2 = M.findWithDefault 0 (x-2) m
-                        a3 = M.findWithDefault 0 (x-3) m
-                        sum' = a1 + a2 + a3
+                        sum' = sum $ map (\z -> M.findWithDefault 0 (x-z) m) [1..3]
                         new_map = M.insert x sum' m
 
 main = do
