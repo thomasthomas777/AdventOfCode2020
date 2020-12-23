de81884d3ba44d63bc9080b71d430a7b96229172
Tom Roadnight <thomasroadnight@gmail.com>
2020-12-23 12:41:31 +0000

    Day 23. Initally submitted Part B solution in Python due to inital stack overflow in Haskell due to no mutability. Commit contains a more optimized implementation for Part B in Haskell.

---
+++ src/Day23/PartA.hs
@@ -0,0 +1,28 @@
+import System.IO
+import Data.List
+import Data.Maybe (fromJust)
+import Data.Char (intToDigit)
+import Data.List.Split (splitOn)
+
+determineInsertion :: [Int] -> [Int] -> Int -> Int -> Int
+determineInsertion picked rest curr alt'
+    | exists_picked || not_exists_rest = determineInsertion picked rest dest alt'
+    | otherwise = dest
+        where   max_elem = foldl1 max (alt':rest)
+                dest = (curr - 1) `mod` (max_elem+1)
+                exists_picked = dest `elem` picked
+                not_exists_rest = not (dest `elem` rest)
+
+iterateMove :: [Int] -> Int -> Int -> String
+iterateMove xs iter max_iter
+    | iter == max_iter  = let idx = fromJust $ elemIndex 1 xs in map intToDigit ((drop (idx+1) xs) ++ (take (idx) xs))
+    | otherwise         = iterateMove new_xs (iter+1) max_iter
+                            where   curr = xs !! 0
+                                    picked = take 3 (drop (1) xs)
+                                    rest = drop 4 xs
+                                    insert' = determineInsertion picked rest curr curr
+                                    insert_idx = fromJust $ elemIndex insert' rest
+                                    new_xs = (take (insert_idx + 1) rest) ++ picked ++ (drop (insert_idx + 1) rest) ++ [curr]
+                    
+main = do
+    print $ iterateMove [2, 1, 5, 6, 9, 4, 7, 8, 3] 0 100
---
+++ src/Day23/PartB.hs
@@ -0,0 +1,41 @@
+import System.IO
+import Data.List
+import Data.Maybe (fromJust)
+import qualified Data.Map as M
+
+type MapType = M.Map Int Int
+
+prepareInput :: [Int] -> (MapType, Int)
+prepareInput xs =   let shifted = (tail xs) ++ [head xs]
+                        zip' = zip xs shifted
+                            in ((foldl' (\x (a, b) -> M.insert a b x) M.empty zip'), head xs)
+
+determineInsertion :: Int -> [Int] -> Int -> Int
+determineInsertion l picked 1 = determineInsertion l picked (l+1)
+determineInsertion l picked curr = let dest = (curr-1) in case (dest `elem` picked) of
+                                                                    True -> determineInsertion l picked dest
+                                                                    False -> dest
+
+
+move :: (MapType, Int) -> Int -> (MapType, Int)
+move (m, iter) _ = let  (Just p1) = M.lookup iter m
+                        (Just p2) = M.lookup p1 m
+                        (Just p3) = M.lookup p2 m
+                        (Just tail_val) = M.lookup p3 m
+                        insert' = determineInsertion (length m) [p1, p2, p3] iter
+                        (Just insert_val) = M.lookup insert' m
+                        m' = M.insert iter tail_val m
+                        m'' = M.insert p3 insert_val m'
+                        m''' = M.insert insert' p1 m''
+                            in (m''', tail_val)
+    
+-- Using the recursive approach for incrementing the iteration from Part A, caused a stack overflow exception to be raised.
+-- To resolve, the strict foldl implementation from an outer function was leveraged to reduce the number of recursive calls.
+iterateMove :: (MapType, Int) -> Int -> Int
+iterateMove (m, a) max_iter = let   (m', aa) = foldl' (\x y -> (move x y)) (m, a) [1..(max_iter)]
+                                    fst = M.findWithDefault 0 1 m'
+                                    snd = M.findWithDefault 0 fst m'
+                                        in fst * snd
+
+main = do
+    print $ iterateMove (prepareInput ([2, 1, 5, 6, 9, 4, 7, 8, 3] ++ [10..1000000])) 10000000
